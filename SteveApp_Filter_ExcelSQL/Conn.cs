12fbc78fc00aadf5884bbf75995647850351656e
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.net>
2022-02-19 16:25:33 +0300

    localdb search grid

---
+++ SteveApp_Filter_ExcelSQL/Conn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteveApp_Filter_ExcelSQL
+{
+    public class Conn
+    {
+        //local git test.
+        public static SqlConnection dbLocal = new SqlConnection();
+        public static string LocalDataSource = "(LocalDb)\\MSSQLLocalDB";
+        public static string LocalDatabase = "SteveAppDB";
+        //public static string LocalAttachDbFilename = "|DataDirectory|\\BLGDB.mdf"; 
+        public static string LocalAttachDbFilename = @"C:\Users\cenk\source\repos\SteveApp_Filter_ExcelSQL\SteveApp_Filter_ExcelSQL\bin\Debug\SteveAppDB.mdf";
+        public static bool ConnDBLocal(bool Connection)
+        {
+            try
+            {
+                if (Connection)
+                {
+                    string conn = "data source=" + Conn.LocalDataSource + ";AttachDbFilename=" + Conn.LocalAttachDbFilename + ";Initial Catalog=" + Conn.LocalDatabase + ";Integrated Security=True;";
+                    Conn.dbLocal.ConnectionString = conn;
+                    Conn.dbLocal.Open();
+                    return true;
+                }
+                else
+                {
+                    Conn.dbLocal.Close();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //ExceptionMessage = ex.Message;
+                return false;
+            }
+        }
+    }
+}
--- SteveApp_Filter_ExcelSQL/Form1.cs
+++ SteveApp_Filter_ExcelSQL/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,27 @@ namespace SteveApp_Filter_ExcelSQL
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Conn.ConnDBLocal(true);
 
+            //Cursor.Current = Cursors.WaitCursor;
+            SqlCommand sqlCmd = new SqlCommand("S_Output", Conn.dbLocal);
+            sqlCmd.CommandTimeout = 30000;
+            //sqlCmd.Parameters.AddWithValue("@ID", ComapnyID);
+            //sqlCmd.Parameters.AddWithValue("@Cell", cell);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = sqlCmd;
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            //gridControl1.DataSource = ds;
+            //gcDashboard.ItemsSource = ds;
+
+            Conn.ConnDBLocal(false);
+
+            gridControl1.DataSource = dt;
         }
     }
 }
